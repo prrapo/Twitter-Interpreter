59a9bcd590cb12e8a50498935a4bd9ad5b97dec8
prrapo <pandirrapo@gmail.com>
2015-12-02 22:32:25 -0500

    LanguageTool Dependencies

--- src/Twitter_Interpreter.java
+++ src/Twitter_Interpreter.java
@@ -1,8 +1,10 @@
+import org.languagetool.JLanguageTool;
+import language.AmericanEnglish;
 
 public class Twitter_Interpreter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		JLanguageTool langTool = new JLanguageTool(new AmericanEnglish());
 
 	}
 
