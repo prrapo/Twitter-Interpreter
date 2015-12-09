c633699dca48f3d3171524b0a2ee8d6fc1fa9ffb
AsaAxelrod <asaaxelrod@gmail.com>
2015-12-09 11:49:45 -0500

    Update Trainer.java

--- src/Trainer.java
+++ src/Trainer.java
@@ -2,6 +2,8 @@ public class Trainer {
 	double inputs[];
 	int answer;
 	
+	//this is the trainer I was using to test the perceptron, it takes in two inputs, the answer, and sets the bias to 1
+	//its very easy to change the number of inputs to any amount necessary
 	public Trainer(double x, double y, int a){
 		inputs = new double [3];
 		inputs[0] = x;
