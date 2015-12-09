bf080343a4abf2b68c00f11833421f17579212a3
AsaAxelrod <asaaxelrod@gmail.com>
2015-12-09 11:46:52 -0500

    Create Trainer.java

---
+++ src/Trainer.java
@@ -0,0 +1,12 @@
+public class Trainer {
+	double inputs[];
+	int answer;
+	
+	public Trainer(double x, double y, int a){
+		inputs = new double [3];
+		inputs[0] = x;
+		inputs[1] = y;
+		inputs[2] = 1;
+		answer = a;
+	}
+}
