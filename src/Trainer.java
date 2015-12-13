44c602676ab68e652a95aacffc92814722b2e0f3
AsaAxelrod <asaaxelrod@gmail.com>
2015-12-13 17:47:50 -0500

    Update Trainer.java

--- src/Trainer.java
+++ src/Trainer.java
@@ -6,12 +6,21 @@ public class Trainer {
 			double[] inputs = c.convertFromWord(input);
 			double[] outputs = c.desiredNumbers(output);
 			network.train(inputs, outputs);
-			for(double o: network.outputLayer.getOutputs()){
-				System.out.print(c.convertToChar(o));
-			}
-			System.out.println();
 		}
-		
+	}
+	
+	public Trainer(NeuralNetwork network, int iterations, String input, String output, boolean print){
+		WordConverter c = new WordConverter();
+		for(int i = 0; i < iterations; i++){
+			double[] inputs = c.convertFromWord(input);
+			double[] outputs = c.desiredNumbers(output);
+			network.train(inputs, outputs);
+			if(print){
+				for(double o: network.outputLayer.getOutputs()){
+					System.out.print(c.convertToChar(o));
+				}
+				System.out.println();
+			}
+		}
 	}
 }
-
